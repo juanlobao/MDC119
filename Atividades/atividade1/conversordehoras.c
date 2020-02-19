ff9ff157102831bc7041009248239f9ecedbe106
unknown <1922082022@academico.iesb.br>
2020-02-19 11:13:59 -0300

    atividade 1  questao 1

--- Atividades/atividade1/conversordehoras.c
+++ Atividades/atividade1/conversordehoras.c
@@ -7,11 +7,12 @@
         int horas = segundos/3600 ;
         segundos = segundos%3600;
         int minutos = segundos/60;
+        segundos = segundos%60;
 
-    printf("Sao %d horas , %d minutos, %d segundos\n,horas,minutos,segundos");
+    printf("Sao %d horas , %d minutos, %d segundos\n",horas,minutos,segundos);
     
     return 0 ;
-    
-
-
     }
+
+
+    
