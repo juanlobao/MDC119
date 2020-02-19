a8983e7643d5a6784ccb665945ab7794f2596a40
unknown <1922082022@academico.iesb.br>
2020-02-19 11:09:03 -0300

    atividade 1  questao 1

---
+++ Atividades/atividade1/conversordehoras.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+    int main () {
+        int segundos = 3000;
+        int horas = segundos/3600 ;
+        segundos = segundos%3600;
+        int minutos = segundos/60;
+
+    printf("Sao %d horas , %d minutos, %d segundos\n,horas,minutos,segundos");
+    
+    return 0 ;
+    
+
+
+    }
